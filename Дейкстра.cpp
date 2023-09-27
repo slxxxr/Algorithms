0c598ca301d79339907b56f03683be74202d5d1c
Luka <98267810+slxxxr@users.noreply.github.com>
2023-09-27 17:31:09 +0300

    Update and rename Dijkstra.cpp to Дейкстра.cpp

---
+++ Дейкстра.cpp
@@ -0,0 +1,18 @@
+vector<int> dijkstra(int s) {
+    vector<int> d(n, inf);
+    d[root] = 0;
+    set< pair<int, int> > q;
+    q.insert({0, s});
+    while (!q.empty()) {
+        int v = q.begin()->second;
+        q.erase(q.begin());
+        for (auto [u, w] : g[v]) {
+            if (d[u] > d[v] + w) {
+                q.erase({d[u], u});
+                d[u] = d[v] + w;
+                q.insert({d[u], u});
+            }
+        }
+    }
+    return d;
+}
