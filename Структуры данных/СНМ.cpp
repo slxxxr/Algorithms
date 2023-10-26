fa8cddf151ca1e8b0bebe138a07426974e6f384b
Bessarab123 <47276297+Bessarab123@users.noreply.github.com>
2023-10-26 19:47:44 +0300

    Update СНМ.cpp

--- Структуры данных/СНМ.cpp
+++ Структуры данных/СНМ.cpp
@@ -18,3 +18,20 @@ void init(int n) {
     for (int i = 0; i < n; i++)
         p[i] = i, s[i] = 1;
 }
+//-----------------------------------------
+
+vector <int> p(n);
+vector <int> size_set(n, 1);
+int get(int x) {
+    if (p[x] != x) p[x] = get(p[x]);
+    return p[x];
+}
+
+
+void union_set(int x, int y) {
+    x = get(x); y = get(y);
+    if (size_set[y] < size_set[x]) swap(x, y);
+    p[x] = y;
+    size_set[y] += size_set[x];
+    //size_set[y] = size_set[x];
+}
