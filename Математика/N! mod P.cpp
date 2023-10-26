4397a85a5a00e977248759304694765c974fb1cc
Luka <98267810+slxxxr@users.noreply.github.com>
2023-10-26 21:28:26 +0300

    Create N! mod P.cpp

---
+++ Математика/N! mod P.cpp
@@ -0,0 +1,11 @@
+N!(без учета чисел, кратных P) mod P, за P*log_p(N)
+ll factmod(ll n, ll p) {
+	ll res = 1;
+	while (n > 1) {
+		res = (res * ((n / p) % 2 ? p - 1 : 1)) % p;
+		for (int i = 2; i <= n % p; ++i)
+			res = (res * i) % p;
+		n /= p;
+	}
+	return res % p;
+}
