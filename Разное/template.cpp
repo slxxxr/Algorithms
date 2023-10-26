3347016230f2b8decf2c737760e1d990bf5fa337
Luka <98267810+slxxxr@users.noreply.github.com>
2023-10-26 16:08:12 +0300

    Добавил вывод дробных чисел

--- Разное/template.cpp
+++ Разное/template.cpp
@@ -33,7 +33,8 @@ const ll inf = 1e18;
 mt19937 rnd(4321);
 //mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 ll random(ll l, ll r) { return (rnd() % (++r - l) + l); } //random in [l...r]
-
+/*cout.setf(ios_base::fixed); - убрать экспоненциальный вывод.
+cout.precision(10); - вывод с заданной точностью */
 
 int main() {
     ios::sync_with_stdio(0);
