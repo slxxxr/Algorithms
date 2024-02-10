867b37c9fda47762d506a12d2e5de4f8361f6085
Luka <98267810+slxxxr@users.noreply.github.com>
2024-02-10 21:42:09 +0300

    Update 04.Dijkstra.cpp

--- 3. Graphs/04.Dijkstra.cpp
+++ 3. Graphs/04.Dijkstra.cpp
@@ -1,18 +1,26 @@
-vector<int> dijkstra(int s) {
-    vector<int> d(n, inf);
-    d[root] = 0;
-    set< pair<int, int> > q;
-    q.insert({0, s});
+vector<vector<pair<ll, ll>>> gr;
+
+vector<ll> dijkstra(int s) {
+    ll n = gr.size();
+    vector<ll> d(n, inf), p(n, -1);
+
+    d[s] = 0;
+    set<pair<ll, ll>> q;
+    q.insert({ 0, s });
     while (!q.empty()) {
-        int v = q.begin()->second;
+        ll v = q.begin()->second;
         q.erase(q.begin());
-        for (auto [u, w] : g[v]) {
-            if (d[u] > d[v] + w) {
-                q.erase({d[u], u});
-                d[u] = d[v] + w;
-                q.insert({d[u], u});
+        for (auto edge : gr[v]) {
+            ll to = edge.first;
+            ll len = edge.second;
+            if (d[v] + len < d[to]) {
+                q.erase({ d[to], to });
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.insert({ d[to], to });
             }
         }
     }
+
     return d;
 }
