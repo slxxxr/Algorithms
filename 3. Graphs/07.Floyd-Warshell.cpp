9f3f343799ac2521c282b57d28e6830a8e490147
Luka <98267810+slxxxr@users.noreply.github.com>
2024-03-14 18:49:14 +0300

    Update 07.Floyd-Warshell.cpp

--- 3. Graphs/07.Floyd-Warshell.cpp
+++ 3. Graphs/07.Floyd-Warshell.cpp
@@ -1,9 +1,13 @@
 Нахождение кратчайшего расстояния между всеми парами вершин за N^3
 d-матрица смежности. d[i][i]=0
 
-  vector<vll> d(n, vll(n, inf));
-for (int k = 0; k < n; ++k)
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+vector<vll> d(n, vll(n, inf));
+for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (d[i][k] < inf && d[k][j] < inf)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+        }
+    }
+}
  
