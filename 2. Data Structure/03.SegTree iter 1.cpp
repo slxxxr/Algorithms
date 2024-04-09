92db8f25660002ce46638c7a599cabd31e7e90ac
Luka <98267810+slxxxr@users.noreply.github.com>
2024-04-09 21:43:43 +0300

    Переписано на общий случай

--- 2. Data Structure/03.SegTree iter 1.cpp
+++ 2. Data Structure/03.SegTree iter 1.cpp
@@ -1,48 +1,60 @@
 struct segtree {
- 
-	vector<ll> tree;
+
+	struct node { // вынести в глобал, если нужно
+		ll suma;
+	};
+
+	const node zero = { 0 };
+
+	node comb(node& a, node& b) {
+		node res = zero;
+		res.suma += a.suma + b.suma;
+		return res;
+	}
+
+	vector<node> tree;
 	ll n = 1;
- 
+
 	void build(vll& arr) {
-		while (n < arr.size()) { n *= 2; }
+		while (n < (ll)arr.size()) { n *= 2; }
 		//n = arr.size();
-		tree.assign(2 * n - 1, 0);
- 
-		for (ll i = 0; i < arr.size(); i++) {
-			tree[n + i - 1] = arr[i];
+		tree.assign(2 * n - 1, zero);
+
+		for (ll i = 0; i < (ll)arr.size(); i++) {
+			tree[n + i - 1] = { arr[i] };
 		}
 		for (ll i = n - 2; i >= 0; i--) {
-			tree[i] = tree[i * 2 + 1] + tree[i * 2 + 2];
+			tree[i] = comb(tree[i * 2 + 1], tree[i * 2 + 2]);
 		}
 	}
- 
+
 	void update(ll ind, ll NewValue) { // 0-index
 		ind += n - 1;
-		tree[ind] = NewValue;
- 
+		tree[ind] = { NewValue };
+
 		while (ind > 0) {
 			ind = (ind - 1) / 2;
-			tree[ind] = tree[2 * ind + 1] + tree[2 * ind + 2];
+			tree[ind] = comb(tree[2 * ind + 1], tree[2 * ind + 2]);
 		}
 	}
- 
- 
+
+
 	ll sum(ll l, ll r) {// sum[l, r] 0-index
 		l += n - 1;
 		r += n;
-		ll result = 0;
- 
+		node res = zero;
+
 		while (l < r) {
 			if (l % 2 == 0) {
-				result += tree[l++];
+				res = comb(res, tree[l++]);
 			}
 			if (r % 2 == 0) {
-				result += tree[--r];
+				res = comb(res, tree[--r]);
 			}
 			l /= 2;
 			r /= 2;
 		}
-		return result;
+		return res.suma;
 	}
- 
+
 };
