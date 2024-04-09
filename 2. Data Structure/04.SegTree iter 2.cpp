52da84b188f8500686be662c217cce9fcda802e3
Luka <98267810+slxxxr@users.noreply.github.com>
2024-04-09 22:03:20 +0300

    Переписано на общий случай

--- 2. Data Structure/04.SegTree iter 2.cpp
+++ 2. Data Structure/04.SegTree iter 2.cpp
@@ -1,27 +1,39 @@
 struct segtree {
 
-	vector<ll> tree;
+	struct node {
+		ll suma;
+	};
+
+	const node zero = { 0 };
+
+	node comb(node& a, node& b) {
+		node res = zero;
+		res.suma += a.suma + b.suma;
+		return res;
+	}
+
+	vector<node> tree;
 	ll n = 1;
 
 	void build(vll& arr) {
-		while (n < arr.size()) { n *= 2; }
+		while (n < (ll)arr.size()) { n *= 2; }
 		//n = arr.size();
-		tree.assign(2 * n - 1, 0);
+		tree.assign(2 * n - 1, zero);
 
 		for (ll i = 0; i < arr.size(); i++) {
-			tree[n + i - 1] = arr[i];
+			tree[n + i - 1] = { arr[i] };
 		}
 	}
 
 	ll get(ll ind) { //0-index
 		ind += n - 1;
-		ll res = tree[0];
+		node res = tree[0];
 
 		while (ind > 0) {
-			res += tree[ind];
+			res = comb(res, tree[ind]);
 			ind = (ind - 1) / 2;
 		}
-		return res;
+		return res.suma;
 	}
 
 
@@ -29,12 +41,16 @@ struct segtree {
 		l += n - 1;
 		r += n;
 
+		node NewValue = { value };
+
 		while (l < r) {
 			if (l % 2 == 0) {
-				tree[l++] += value;
+				tree[l] = comb(tree[l], NewValue);
+				l++;
 			}
 			if (r % 2 == 0) {
-				tree[--r] += value;
+				r--;
+				tree[r] = comb(tree[r], NewValue);
 			}
 			l /= 2;
 			r /= 2;
