82e4d48552882a99fd7dc155a2ca2cbf84e38294
Luka <98267810+slxxxr@users.noreply.github.com>
2024-07-18 18:57:52 +0300

    Update 09.Euler function.cpp

--- 5. Math/09.Euler function.cpp
+++ 5. Math/09.Euler function.cpp
@@ -1,18 +1,36 @@
-int phi(int n) {
-    int result = n;
-    for (int i = 2; i * i <= n; i++) {
+vll all_phi(ll n) {// Calculate all phi from 1 to N, O(N*log(logN))
+    vector<ll> phi(n + 1);
+    for (ll i = 0; i <= n; i++) {
+        phi[i] = i;
+    }
+
+    for (ll i = 2; i <= n; i++) {
+        if (phi[i] == i) {
+            for (ll j = i; j <= n; j += i) {
+                phi[j] -= phi[j] / i;
+            }
+        }
+    }
+
+    return phi;
+}
+
+ll calc_phi(ll n) { // Calculate phi(n), O(sqrt(N))
+    ll result = n;
+    for (ll i = 2; i * i <= n; i++) {
         if (n % i == 0) {
-            while (n % i == 0)
+            while (n % i == 0) {
                 n /= i;
+            }
             result -= result / i;
         }
     }
-    if (n > 1)
+    if (n > 1) {
         result -= result / n;
+    }
     return result;
 }
 
 /*
-Работает за sqrt(n)
-a^phi(m)===1(mod m) если а и m взаимно простые => a^(phi(m)-1) - обратный элемент к a
+a^phi(m) == 1(mod m) if gcd(a,m) == 1, ==> a^(phi(m)-1) - обратный элемент к a
 */
