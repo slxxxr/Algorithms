d2942e17177fe6cba51fc0bf3df80d342b79e45f
Luka <98267810+slxxxr@users.noreply.github.com>
2024-07-18 18:44:43 +0300

    Create 15.Combinatorics.cpp

---
+++ 5. Math/15.Combinatorics.cpp
@@ -0,0 +1,45 @@
+class Combinatorics {
+
+public:
+    
+    ll get_c(ll k, ll n) { // return C(k,n) = n! / ((n-k)! * k!) , k <= n; 
+        return ((fact[n] * inv_f[n - k]) % mod * inv_f[k]) % mod;
+    }
+
+    ll get_katalan(ll n) { // return Catalan number
+        return get_c(n, 2 * n);
+    }
+
+    void build(ll _maxn, ll _mod) { // Calculate all factorials and inv. factorials, O(N)
+        maxn = _maxn; mod = _mod;
+
+        fact.assign(maxn + 1, 1);
+        inv_f.assign(maxn + 1, 1);
+
+        for (ll i = 1; i <= maxn; i++) {
+            fact[i] = (fact[i - 1] * i) % mod;
+        }
+
+        inv_f[maxn] = binpow(fact[maxn], mod - 2);
+        for (ll i = maxn - 1; i >= 1; i--) {
+            inv_f[i] = (inv_f[i + 1] * (i + 1)) % mod;
+        }
+    }
+
+private:
+    ll maxn, mod;
+    vll fact, inv_f; // 1 - index, Factorial(i) = fact[i]
+    
+    ll binpow(ll a, ll n) {
+        ll res = 1;
+        while (n != 0) {
+            if (n & 1) {
+                res = (res % mod * a % mod) % mod;
+            }
+            a = (a % mod * a % mod) % mod;
+            n >>= 1;
+        }
+        return res;
+    }
+
+};
