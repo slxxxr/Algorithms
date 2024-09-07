3162bf488862da03d96fca78090121da84c3415d
Luka <98267810+slxxxr@users.noreply.github.com>
2024-09-07 17:34:16 +0300

    Update 08.LCA logN.cpp
    Переписано на класс

--- 3. Graphs/08.LCA logN.cpp
+++ 3. Graphs/08.LCA logN.cpp
@@ -1,42 +1,58 @@
+class LCA {
+private:
+    int n, logn, root;
+    vector<int> depth;
+    vector<vector<int>> up;
 
-ll n, m, timer = 0;
-ll l = 1; //log(n)
-vector<vll> gr, up;
-vll tin, tout;
-
-
-void dfs_lca(ll v, ll p = 0) {
-    tin[v] = ++timer;
-    up[v][0] = p;
-
-    for (ll i = 1; i <= l; ++i) {
-        up[v][i] = up[up[v][i - 1]][i - 1];
+    void calc_depth(int v, vector<vector<int>>& gr, int p = -1) {
+        depth[v] = (p == -1 ? 0 : depth[p] + 1);
+        up[v][0] = p;
+        for (ll u : gr[v]) {
+            if (u != p) {
+                calc_depth(u, gr, v);
+            }
+        }
     }
-    
-    for (ll u : gr[v]) {
-        if (u != p)
-            dfs_lca(u, v);
+
+    void binary_lifting() {
+        for (int i = 1; i < logn; i++) { // up[i][0] = parent[i]
+            for (int v = 0; v < n; v++) {
+                up[v][i] = (up[v][i - 1] == -1 ? -1 : up[up[v][i - 1]][i - 1]);
+            }
+        }
     }
-    tout[v] = ++timer;
-}
 
-bool is_ancestor(ll a, ll b) {//проверка a это предок b?
-    return tin[a] <= tin[b] && tout[a] >= tout[b];
-}
+public:
+    LCA(vector<vector<int>>& gr, int _root = 0) { // time: O(N*logN), memory: O(N*logN)
+        n = gr.size();
+        logn = (int)log2(n) + 1;
+        root = _root;
 
-ll lca(ll a, ll b) {
-    if (is_ancestor(a, b))  return a;
-    if (is_ancestor(b, a))  return b;
-    for (ll i = l; i >= 0; --i)
-        if (!is_ancestor(up[a][i], b))
-            a = up[a][i];
-    return up[a][0];
-}
+        depth.assign(n, 0);
+        up.assign(n, vector<int>(logn, -1));
 
-void preprocess(ll root) {
-    tin.resize(n);
-    tout.resize(n);
-    up.assign(n, vll(l + 1));
-    while ((1LL << l) <= n)l++;
-    dfs_lca(root);
-}
+        calc_depth(root, gr, -1);
+        binary_lifting();
+    }
+
+
+    int get_lca(int v, int u) {// time: O(logN)
+        if (depth[v] > depth[u]) { swap(v, u); }
+
+        int delta = depth[u] - depth[v];
+        for (int i = 0, pw = 1; i < logn; i++, pw <<= 1) {
+            if ((pw & delta)) {
+                u = up[u][i];
+            }
+        }
+
+        for (int i = logn - 1; i >= 0; i--) {
+            if (up[v][i] != up[u][i]) {
+                v = up[v][i];
+                u = up[u][i];
+            }
+        }
+
+        return (u == v ? u : up[u][0]);
+    }
+};
