1726d7bc6cbb029d82a27dae997f83002b802a98
Luka <98267810+slxxxr@users.noreply.github.com>
2024-10-23 21:27:01 +0300

    Update 15.Combinatorics.cpp

--- 5. Math/15.Combinatorics.cpp
+++ 5. Math/15.Combinatorics.cpp
@@ -1,47 +1,44 @@
 class Combinatorics {
+private:
+    ll _maxn, _mod;
+    vector<ll> _fact, _inv_f; // 1 - index, Factorial(i) = fact[i]
 
 public:
-    
-    ll get_c(ll k, ll n) { // return C(k,n) = n! / ((n-k)! * k!) , k <= n; 
+
+    ll get_c(int k, int n) { // return C(k,n) = n! / ((n-k)! * k!); O(1)
         if (k > n) { return 0; }
-        return ((fact[n] * inv_f[n - k]) % mod * inv_f[k]) % mod;
+        return ((_fact[n] * _inv_f[n - k]) % _mod * _inv_f[k]) % _mod;
     }
 
-    ll get_katalan(ll n) { // return Catalan number
+    ll get_catalan(int n) { // return Catalan number, O(1)
         return get_c(n, 2 * n);
     }
 
-    Combinatorics(ll _maxn, ll _mod) { // Calculate all factorials and inv. factorials, O(N)
-        maxn = _maxn; mod = _mod;
-
-        fact.assign(maxn + 1, 1);
-        inv_f.assign(maxn + 1, 1);
+    Combinatorics(ll maxn, ll mod) { // Calc all factorials and inv. factorials; O(N)
+        _maxn = maxn; _mod = mod;
+        _fact.assign(maxn + 1, 1);
+        _inv_f.assign(maxn + 1, 1);
 
         for (ll i = 1; i <= maxn; i++) {
-            fact[i] = (fact[i - 1] * i) % mod;
+            _fact[i] = (_fact[i - 1] * i) % mod;
         }
 
-        inv_f[maxn] = binpow(fact[maxn], mod - 2);
+        _inv_f[maxn] = binpow(_fact[maxn], mod - 2); // if mod is prime
         for (ll i = maxn - 1; i >= 1; i--) {
-            inv_f[i] = (inv_f[i + 1] * (i + 1)) % mod;
+            _inv_f[i] = (_inv_f[i + 1] * (i + 1)) % mod;
         }
     }
 
-private:
-    ll maxn, mod;
-    vll fact, inv_f; // 1 - index, Factorial(i) = fact[i]
-    
     ll binpow(ll a, ll n) {
-        a %= mod;
+        a %= _mod;
         ll res = 1;
         while (n != 0) {
             if (n & 1) {
-                res = (res * a) % mod;
+                res = (res * a) % _mod;
             }
-            a = (a * a) % mod;
+            a = (a * a) % _mod;
             n >>= 1;
         }
         return res;
     }
-
 };
