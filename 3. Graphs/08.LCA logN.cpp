5e6159818cf55a715f98ea11ee00f38da86990d0
Luka <98267810+slxxxr@users.noreply.github.com>
2024-10-25 18:18:56 +0300

    Update 08.LCA logN.cpp

--- 3. Graphs/08.LCA logN.cpp
+++ 3. Graphs/08.LCA logN.cpp
@@ -1,13 +1,13 @@
 class LCA {
 private:
-    int n, logn, root;
-    vector<int> depth;
-    vector<vector<int>> up;
+    int _n, _logn, _root;
+    vector<int> _depth;
+    vector<vector<int>> _up;
 
     void calc_depth(int v, vector<vector<int>>& gr, int p = -1) {
-        depth[v] = (p == -1 ? 0 : depth[p] + 1);
-        up[v][0] = p;
-        for (int u : gr[v]) {
+        _depth[v] = (p == -1 ? 0 : _depth[p] + 1);
+        _up[v][0] = p;
+        for (ll u : gr[v]) {
             if (u != p) {
                 calc_depth(u, gr, v);
             }
@@ -15,44 +15,44 @@ private:
     }
 
     void binary_lifting() {
-        for (int i = 1; i < logn; i++) { // up[i][0] = parent[i]
-            for (int v = 0; v < n; v++) {
-                up[v][i] = (up[v][i - 1] == -1 ? -1 : up[up[v][i - 1]][i - 1]);
+        for (int i = 1; i < _logn; i++) { // up[i][0] = parent[i]
+            for (int v = 0; v < _n; v++) {
+                _up[v][i] = (_up[v][i - 1] == -1 ? -1 : _up[_up[v][i - 1]][i - 1]);
             }
         }
     }
 
 public:
-    LCA(vector<vector<int>>& gr, int _root = 0) { // time: O(N*logN), memory: O(N*logN)
-        n = gr.size();
-        logn = (int)log2(n) + 1;
-        root = _root;
+    LCA(vector<vector<int>>& gr, int _root = 0) {// Time: O(NlogN)
+        _n = gr.size();
+        _logn = (int)log2(_n) + 1;
+        _root = _root;
 
-        depth.assign(n, 0);
-        up.assign(n, vector<int>(logn, -1));
+        _depth.assign(_n, 0);
+        _up.assign(_n, vector<int>(_logn, -1));
 
-        calc_depth(root, gr, -1);
+        calc_depth(_root, gr, -1);
         binary_lifting();
     }
 
 
-    int get_lca(int v, int u) {// time: O(logN)
-        if (depth[v] > depth[u]) { swap(v, u); }
+    int get_lca(int v, int u) {// Time: O(logN)
+        if (_depth[v] > _depth[u]) { swap(v, u); }
 
-        int delta = depth[u] - depth[v];
-        for (int i = 0, pw = 1; i < logn; i++, pw <<= 1) {
+        int delta = _depth[u] - _depth[v];
+        for (int i = 0, pw = 1; i < _logn; i++, pw <<= 1) {
             if ((pw & delta)) {
-                u = up[u][i];
+                u = _up[u][i];
             }
         }
 
-        for (int i = logn - 1; i >= 0; i--) {
-            if (up[v][i] != up[u][i]) {
-                v = up[v][i];
-                u = up[u][i];
+        for (int i = _logn - 1; i >= 0; i--) {
+            if (_up[v][i] != _up[u][i]) {
+                v = _up[v][i];
+                u = _up[u][i];
             }
         }
 
-        return (u == v ? u : up[u][0]);
+        return (u == v ? u : _up[u][0]);
     }
 };
