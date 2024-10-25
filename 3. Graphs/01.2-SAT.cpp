d82a48a8a132f3bb48dc2ba7c383dc6f5f920e68
Luka <98267810+slxxxr@users.noreply.github.com>
2024-10-25 18:31:58 +0300

    Update 01.2-SAT.cpp

--- 3. Graphs/01.2-SAT.cpp
+++ 3. Graphs/01.2-SAT.cpp
@@ -1,27 +1,33 @@
-ll n, m, cnt_perem;
-vector<vector<ll>> gr, gr_obr;
-vll used, order, comp;
+/* a xor b = (a or b) and (!a or !b) = (!a->b) and (!b->a) and (a->!b) and (b->!a)
+a or b = (!a->b) and (!b->a)
+a and b = (a or b) and (a or !b) and (!a or b) = (!a->b) and (!b->a) and (!a->!b) and (b->a) and (a->b) and (!b->!a)
+!(a xor b) = (a or !b) and (!a or b) = (!a->!b) and (b->a) and (a->b) and (!b->!a)
+!(a and b) = (!a or !b) = (a->!b) and (b->!a)
+!(a or b) = (a or !b) and (!a or b) and (!a or !b) = (!a->!b) and (b->a) and (a->b) and (!b->!a) and (a->!b) and (b->!a) */
 
-void top_sort(ll v) {
+int n, m, cnt_perem;
+vector<vector<int>> gr, gr_obr;
+vector<int> used, order, comp;
+
+void top_sort(int v) {
     used[v] = 1;
-    for (ll u : gr[v]) {
+    for (int u : gr[v]) {
         if (!used[u]) top_sort(u);
     }
     order.pb(v);
 }
 
-void paint(ll v, ll color) {
+void paint(int v, int color) {
     comp[v] = color;
-    for (ll u : gr_obr[v]) {
+    for (int u : gr_obr[v]) {
         if (comp[u] == -1) paint(u, color);
     }
 }
 
-
 void create_obr() {
     gr_obr.resize(2 * cnt_perem);
-    for (ll i = 0; i < 2 * cnt_perem; i++) {
-        for (ll j : gr[i]) {
+    for (int i = 0; i < 2 * cnt_perem; i++) {
+        for (int j : gr[i]) {
             gr_obr[j].pb(i);
         }
     }
@@ -29,23 +35,23 @@ void create_obr() {
 
 void find_sort() {
     used.assign(2 * cnt_perem, 0);
-    for (ll i = 0; i < 2 * cnt_perem; i++) {
+    for (int i = 0; i < 2 * cnt_perem; i++) {
         if (!used[i]) top_sort(i);
     }
     reverse(all(order));
 }
 
 void paint_comp() {
-    ll color = 0;
+    int color = 0;
     comp.assign(2 * cnt_perem, -1);
-    for (ll i : order) {
+    for (int i : order) {
         if (comp[i] == -1) paint(i, color++);
     }
 }
 
-void solve_2sat() {
+void solve_2sat() { // Time: O(N + M)
     create_obr();
     find_sort();
     paint_comp();
-    //Если comp[a]==comp[a+cnt_perem] ==> нет решения
+    //If comp[a] == comp[a+cnt_perem] ==> no solution
 }
